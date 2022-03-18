72cc057ee11b0ec42bc228ef812e130983cc0b81
josiahwebb <jj.webb520@gmail.com>
2022-03-17 20:03:17 -0600

    cycles thru enemies, displays end game message, ui timing fix

--- index.js
+++ index.js
@@ -3,38 +3,51 @@ import Character from "./Character.js"
 
 const monstersArr = ['orc', 'goblin', 'demon']
 const wizard = new Character(characterData.hero)
-// const monster = getNewMonster()
+let monster = getNewMonster()
 
 function getNewMonster() {
    const nextMonsterData = characterData[monstersArr.shift()]
-   return (!nextMonsterData) ? new Character(nextMonsterData) : {}
+   return (nextMonsterData) ? new Character(nextMonsterData) : {}
 }
 
-const nextMonsterData = () => new Character(characterData[monstersArr[0]])
+// const nextMonsterData = () => {
+//    return (monstersArr[0]) ? new Character(characterData[monstersArr[0]]) : {}
+// }
+
 
 function render() {
    document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
-   document.getElementById('monster').innerHTML = nextMonsterData().getCharacterHtml()
+   document.getElementById('monster').innerHTML = monster.getCharacterHtml()
 }
 
 render()
 
 function attack() {
+   // document.querySelector('#attack-button').removeEventListener('click', attack)
+   // setTimeout(() => {
+   //    document.querySelector('#attack-button').addEventListener('click', attack)
+   // }, 2000)
+   pauseAttackBtn()
    wizard.getDiceHtml()
-   orc.getDiceHtml()
-   wizard.takeDamage(orc.currentDiceScore)
-   orc.takeDamage(wizard.currentDiceScore)
+   monster.getDiceHtml()
+   wizard.takeDamage(monster.currentDiceScore)
+   monster.takeDamage(wizard.currentDiceScore)
    render()
-   if(orc.isDead || wizard.isDead) {
-      endGame()
+   if(wizard.isDead) return setTimeout(endGame, 1500);
+   if(monstersArr.length == 0 && monster.isDead) return setTimeout(endGame, 1500);
+   if(monster.isDead && monstersArr.length > 0){
+      monster = getNewMonster();
+      setTimeout(render, 1500)
    }
 }
 
 function endGame() {
-   const endMessage = orc.isDead && wizard.isDead ? 'No victors - all dead'
-                     : orc.isDead ? 'The wizard wins'
-                     : 'The orc pulled it off'
-   const endEmoji = (wizard.isDead) ? "💀" : "🔮"
+   const endMessage = monster.isDead && wizard.isDead ? 'No victors - total death'
+                     : monster.isDead ? 'The wizard wins'
+                     : `The evil ${monster.name} conquers`
+   const endEmoji = monster.isDead && wizard.isDead ? "💀" 
+                     : monster.isDead ? "🔮"
+                     : "😈"
    const endingHtml = `<div class="end-game">
                            <h2>Game Over</h2>
                            <h3>${endMessage}</h3>
@@ -44,3 +57,10 @@ function endGame() {
 }
 
 document.querySelector('#attack-button').addEventListener('click', attack)
+
+function pauseAttackBtn() {
+   document.querySelector('#attack-button').removeEventListener('click', attack)
+   setTimeout(() => {
+      document.querySelector('#attack-button').addEventListener('click', attack)
+   }, 1600)
+}
--- utils.js
+++ utils.js
@@ -1,13 +1,13 @@
 function getDiceRollsArr (diceCount) {
-    return new Array(diceCount).fill().map(i => {
-        return Math.floor(Math.random() * 6 + 1)
-    })
+    return new Array(diceCount).fill().map(() =>
+        Math.floor(Math.random() * 6 + 1)
+    )
 }
 
 function getDicePlaceholderHtml(dice) {
-    return new Array(dice).fill().map(i => {
-       return `<div class="placeholder-dice"></div>`
-    }).join('')
+    return new Array(dice).fill().map(() => 
+        `<div class="placeholder-dice"></div>`
+    ).join('')
 }
 
 const getPercentage = (maximumHealth, remainingHealth) => Math.round(remainingHealth / maximumHealth * 100)
